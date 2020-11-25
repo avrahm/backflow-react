a3a6b4e61ab7ebe508fc1bfdf2f5ee6169a90265
Avrahm <avrahmk@me.com>
2020-11-24 22:03:17 -0500

    browser router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
+import Home from './Components/Home';
 import NavBar from './Components/NavBar';
+import Services from './Components/Services';
+
 
 function App() {
   return (
-      <NavBar/>
+    <BrowserRouter>
+      <NavBar />
+
+      <Switch>
+          <Route path="/services">
+            <Services />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+      </Switch>
+
+    </BrowserRouter>
   );
 }
 
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -9,28 +9,28 @@ export default function NavBar() {
   return (
     <>
     <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
-      <Navbar.Brand href="index.html#">
+      <Navbar.Brand href="/">
         <img src={logo} width="150" height="60" />
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link href="#index.html">Home</Nav.Link>
+          <Nav.Link href="/">Home</Nav.Link>
           <NavDropdown title="Services" id="collasible-nav-dropdown">
-            <NavDropdown.Item href="services.html#testing">
+            <NavDropdown.Item href="/services">
               Backflow Testing
             </NavDropdown.Item>
-            <NavDropdown.Item href="services.html#repairs">
+            <NavDropdown.Item href="/services">
               Backflow Repairs
             </NavDropdown.Item>
-            <NavDropdown.Item href="services.html#installation">
+            <NavDropdown.Item href="/services">
               Backflow Installation
             </NavDropdown.Item>
             <NavDropdown.Divider />
             {/* <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item> */}
           </NavDropdown>
-          <Nav.Link href="#locations.html">Locations</Nav.Link>
-          <Nav.Link href="#contactus.html">Contact Us</Nav.Link>
+          <Nav.Link href="/locations">Locations</Nav.Link>
+          <Nav.Link href="/contactus">Contact Us</Nav.Link>
         </Nav>
         <Nav>
           <Nav.Link eventKey={2} href="tel:706-438-2522">
