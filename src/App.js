557fdb6d03b9ecebe41a4e9d476e33a68e9b259b
Avrahm <avrahmk@me.com>
2020-11-26 21:59:07 -0500

    app js routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
-import Home from './Components/Home';
-import NavBar from './Components/NavBar';
-import Services from './Components/Services';
+import Footer from './Components/Footer/Footer';
+import Home from './Components/Home/Home';
+import NavBar from './Components/NavBar/NavBar';
+import Services from './Components/Services/Services';
+import WhatIsBackflow from './Components/WhatIsBackflow/WhatIsBackflow';
+import Locations from './Components/Locations/Locations';
+import ContactUs from './Components/ContactUs/ContactUs';
 
 
 function App() {
@@ -11,14 +15,24 @@ function App() {
       <NavBar />
 
       <Switch>
-          <Route path="/services">
-            <Services />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
+        <Route path="/services">
+          <Services />
+        </Route>
+        <Route path="/whatisbackflow">
+          <WhatIsBackflow />
+        </Route>
+        <Route path="/locations">
+          <Locations />
+        </Route>
+        <Route path="/contactus">
+          <ContactUs />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
       </Switch>
-
+      
+      <Footer />
     </BrowserRouter>
   );
 }
